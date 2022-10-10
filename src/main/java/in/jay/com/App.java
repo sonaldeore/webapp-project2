5abda2b5154367e12c9d443f02c307f0e82af38d
sonal <sonal@gmail.com>
2022-10-10 05:56:17 +0000

    app java added

---
+++ src/main/java/in/jay/com/App.java
@@ -0,0 +1,13 @@
+package .in.jay.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World wLCOME TO THIS SESSION!" );
+    }
+}
